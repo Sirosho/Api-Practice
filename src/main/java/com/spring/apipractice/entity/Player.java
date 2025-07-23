3d95cba03432064fcf168c3e325db5bb3aa10997
Sirosho <pack8447@gmail.com>
2025-07-23 14:01:39 +0900

    ApiPractice 생성

--- src/main/java/com/spring/apipractice/entity/Player.java
+++ src/main/java/com/spring/apipractice/entity/Player.java
@@ -2,6 +2,8 @@ package com.spring.apipractice.entity;
 
 import lombok.*;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -10,7 +12,6 @@ import java.time.LocalDateTime;
 @ToString
 @EqualsAndHashCode
 @NoArgsConstructor
-@AllArgsConstructor
 @Builder
 public class Player {
 
@@ -20,5 +21,30 @@ private int age;
 private Position position;
 private double war;
 private LocalDateTime createdAt;
+
+    public Player(ResultSet rs)throws SQLException {
+        String posName = rs.getString("position");
+        this.id = rs.getLong("id");
+        this.name = rs.getString("name");
+        this.age = rs.getInt("age");
+        this.position = getPos(posName);
+        this.war = rs.getDouble("war");
+        this.createdAt = rs.getTimestamp("created_at").toLocalDateTime();
+    }
+
+
+    // db의 Enum 값을 Java의 enum으로 변환하는 매서드
+    public Position getPos(String pos){
+
+        for (Position position : Position.values()) {
+
+            if(position.getPosName().equals(pos))
+                return position;
+
+        }
+
+
+    }
+
 
 }
